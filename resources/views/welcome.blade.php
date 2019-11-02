5783fe34e6d5eabb2d49eaf3244782b5a5daceea
laraibIshtiaq <hammadbhatti634@gmail.com>
2019-11-02 13:59:53 +0500

    'readme.md'

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laraibia Mobile Store 
+                    Laraibia Mobile Store  
                 </div>
 
                 <div class="links">
