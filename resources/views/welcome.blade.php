fc1d3fc70c1812b7db089c32637364be9255f4ba
laraibIshtiaq <hammadbhatti634@gmail.com>
2019-11-09 18:17:51 +0500

    Init commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,19 @@
+@extends('layout')
+@section('title', 'Welcome 5B')
+
+@section('navbar')
+@parent
+@endsection
+
+@section('content')
+    <div class="jumbotron">
+      <h1>Welcome to Class</h1>      
+      <p>Good luck for your sessional</p>
+      </div>
+   </div>
+@endsection 
+
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -5,7 +21,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,4 @@
+
 <?php
 
 /*
@@ -10,11 +11,29 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('123','Hellocontroller@gettnigvale');
+Route::post('/submit','Hellocontroller@index');
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Route::get('/welcome', function () {
+    return view('welcome');
+});
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/registration', function () {
+    return view('registration');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/contacts', function () {
+    return view('contacts');
+});
+Route::get('data/{id}',function($id) {
+   echo 'ID: '.$id;
+});
+
+
+?>
